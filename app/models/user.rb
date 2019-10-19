c2a717e9488a57c7693cdfb96a4670f63e02069d
Pavani Pamarthy <pavani.pamarthy52@gmail.com>
2019-10-19 01:20:26 +0000

    Models updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many: white_games
+  has_many: white_games, :foreign_key=> white_user_id, :class_name=> games
   has_many: pieces
-  has_many: black_games
+  has_many: black_games, :foreign_key=> white_user_id, :class_name=> games
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/migrate/20191018210031_create_games.rb
+++ db/migrate/20191018210031_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-      t.integer :game_id
+    
       t.integer :white_user_id
       t.integer :black_user_id
       t.timestamps
