fab1c1673bdf1cdbf5810185e06bc9544acd0f3d
Pavani Pamarthy <pavani.pamarthy52@gmail.com>
2019-10-26 01:23:15 +0000

    crud

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,24 @@
 class GamesController < ApplicationController
   def index
   end
+
+  def new
+    @game= Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+  end
+
+  def show
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(
+      :white_player_id,
+      :black_player_id)
+  end
+
 end
